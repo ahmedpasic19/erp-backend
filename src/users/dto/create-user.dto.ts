8bb5fcfc244f179a67f2fa1562e81cc477c5ee7b
ahmedpasic19 <ahmedpasic15@gmail.com>
2024-02-21 20:13:43 +0100

    ERP-85 CRUD Clients
    Changes:
    - ENUM user type
    - Create client API
    - GET all compaies clients

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -10,11 +10,13 @@ export const CreateUserDto = z.object({
     .min(8, 'Minimum 8 characters')
     .max(45, 'Maximum 45 characters'),
   email: z.string().email('Not valid email format'),
-  companies: z.array(
-    z.object({
-      company_id: z.coerce.number().min(1, 'Must provide companyId'),
-    }),
-  ),
+  companies: z
+    .array(
+      z.object({
+        company_id: z.coerce.number().min(1, 'Must provide companyId'),
+      }),
+    )
+    .nullish(),
 });
 
 export type CreateUserDto = z.infer<typeof CreateUserDto>;
